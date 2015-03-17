e75f188befc451f721c09d6515a3e7c08936c6b1
jmoulin <jmoulin@85223511-97c2-6742-acd2-766018259031>
2015-03-17 15:22:50 +0000

    factorized autoload
    git-svn-id: https://svn.castelis.com/svn/FMUP@24 85223511-97c2-6742-acd2-766018259031

--- trunk/system/framework.php
+++ trunk/system/framework.php
@@ -19,47 +19,7 @@ if (!isset($_GET['sys'])) {
  **/
 date_default_timezone_set("Europe/Paris");
 
-// pour la manipulation des urls
-require_once 'string.php';
-
-// Déclaration de l'autoloader pour phpunit
-spl_autoload_register('__autoload');
-
-/**
- * Chargement automatique des classes
- * Les classes sytème ont priorité
- * puis les classes du modèle
- * puis les helpers
- **/
-function __autoload($class_name)
-{
-    $class_name = strtolower(String::to_Case($class_name));
-
-    //liste des repertoires pouvant contenir une classe à include
-    $classes_path = array(
-        __DIR__,
-        __DIR__ . '/helper',
-        __DIR__ . '/component',
-        __DIR__ . '/../application',
-        __DIR__ . '/../application/model',
-        __DIR__ . '/../application/model/base',
-        __DIR__ . '/../../../../application',
-        __DIR__ . '/../../../../application/model',
-        __DIR__ . '/../../../../application/model/base',
-    );
-
-    //recherche de la classe
-    $include_path = '';
-    foreach ($classes_path as $class_path) {
-        if (file_exists($class_path.'/'.$class_name.'.php')) {
-            $include_path = $class_path.'/'.$class_name.'.php';
-            break;
-        }
-    }
-    
-    //include de la classe
-    if ($include_path) require_once $include_path;
-}
+require_once('autoload.php');
 
 $sys_directory = null;
 $sys_controller = null;
