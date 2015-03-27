eb50cea0eddb69abeac611595f02ade3d5c8535d
jmoulin <jmoulin@85223511-97c2-6742-acd2-766018259031>
2015-03-27 10:10:08 +0000

    add Multiton to Helper Db
    git-svn-id: https://svn.castelis.com/svn/FMUP@48 85223511-97c2-6742-acd2-766018259031

--- trunk/lib/Helper/Db.php
+++ trunk/lib/Helper/Db.php
@@ -7,21 +7,36 @@ namespace FMUP\Helper;
  */
 abstract class Db
 {
+    const DEFAULT_NAME = 'DEFAULT_NAME';
     /**
-     * @var \FMUP\Db
+     * @var array \FMUP\Db
      */
-    protected static $instance;
+    protected static $instances = array();
 
     /**
      * @return \FMUP\Db
      */
-    public static function getInstance()
+    public static function getInstance($name = self::DEFAULT_NAME)
     {
-        if (is_null(self::$instance)) {
-            self::$instance = new \FMUP\Db(\Config::parametresConnexionDb());
+        if (is_null($name)) {
+            throw new \InvalidArgumentException('Name must be set');
+        }
+        $name = (string)$name;
+        if (is_null(self::$instances[$name])) {
+            if ($name == self::DEFAULT_NAME) {
+                $params = \Config::parametresConnexionDb();
+            } else {
+                $dbSettings = \Config::paramsVariables('db');
+                if (isset($dbSettings[$name])) {
+                    $params = $dbSettings[$name];
+                } else {
+                    throw new \OutOfRangeException('Trying to access a database name ' . $name . ' that not exists');
+                }
+            }
+            self::$instances[$name] = new \FMUP\Db($params);
         }
 
-        return self::$instance;
+        return self::$instances[$name];
     }
 
     /**
