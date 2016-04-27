07935309829eb395518b28e840dadecada24aa73
jmoulin <jmoulin@castelis.com>
2016-04-27 14:01:07 +0200

    add Controller\Helper\Download 100% code coverage

---
+++ tests/Controller/Helper/Download.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Download.php
+ * @author: jmoulin@castelis.com
+ */
+
+namespace Tests\Controller\Helper;
+
+use FMUP\Logger;
+
+class MockFail
+{
+    use \FMUP\Controller\Helper\Download;
+}
+
+class Mock extends \FMUP\Controller implements \FMUP\Logger\LoggerInterface
+{
+    private $response;
+    use \FMUP\Controller\Helper\Download;
+    use \FMUP\Logger\LoggerTrait;
+
+    public function __construct()
+    {
+    }
+
+    public function hasResponse()
+    {
+        return true;
+    }
+
+    public function getResponse()
+    {
+        if (!$this->response) {
+            $this->response = new \FMUP\Response();
+        }
+        return $this->response;
+    }
+}
+
+class DownloadTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDownloadHeadersFailsWhenNotController()
+    {
+        $this->expectException(\FMUP\Exception::class);
+        $this->expectExceptionMessage('Unable to use Download trait');
+        (new MockFail())->downloadHeaders('text/html');
+    }
+
+    public function testDownloadFailsWhenNotController()
+    {
+        $this->expectException(\FMUP\Exception::class);
+        $this->expectExceptionMessage('Unable to use Download trait');
+        (new MockFail())->download('/not/existing/path');
+    }
+
+    public function testDownloadFailsWhenFileNotExists()
+    {
+        $this->expectException(\FMUP\Exception\Status\NotFound::class);
+        $this->expectExceptionMessage('Unable to find requested file');
+        $logger = $this->getMock(\FMUP\Logger::class, array('log'));
+        $logger->expects($this->exactly(1))
+            ->method('log')
+            ->with(
+                $this->equalTo(\FMUP\Logger\Channel\System::NAME),
+                $this->equalTo(\FMUP\Logger::ERROR),
+                $this->equalTo('Unable to find requested file'),
+                $this->equalTo(array('filePath' => '/not/existing/path'))
+            );
+        /** @var \FMUP\Logger $logger */
+        (new Mock())->setLogger($logger)->download('/not/existing/path');
+    }
+
+    public function testDownload()
+    {
+        $file = implode(DIRECTORY_SEPARATOR, array(__DIR__, '..', '..', '..', 'composer.json'));
+        $expectedContent = file_get_contents($file);
+        $response = $this->getMock(\FMUP\Response::class, array('send', 'clearHeader'));
+        $response->expects($this->exactly(1))->method('send');
+        $response->expects($this->exactly(1))->method('clearHeader');
+        $mock = $this->getMock(Mock::class, array('downloadHeaders', 'getResponse', 'obFlush'));
+        $mock->expects($this->exactly(1))->method('downloadHeaders')->willReturn($response);
+        $mock->method('getResponse')->willReturn($response);
+        /** @var $mock Mock */
+        $this->expectOutputString($expectedContent);
+        $mock->download($file);
+    }
+
+    public function testDownloadHeaders()
+    {
+        $this->assertInstanceOf(\FMUP\Response::class, (new Mock)->downloadHeaders('text/html'));
+    }
+}
