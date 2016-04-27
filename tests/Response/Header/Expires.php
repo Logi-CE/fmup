88d79ef64dd67b8892f6e466d0eb8bdae35cf0b7
jmoulin <jmoulin@castelis.com>
2016-04-27 16:10:08 +0200

    add Response\Header\Expires 100% code coverage

---
+++ tests/Response/Header/Expires.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Expires.php
+ * @author: jmoulin@castelis.com
+ */
+
+namespace Tests\Response\Header;
+
+
+class ExpiresTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $expires = new \FMUP\Response\Header\Expires();
+        $this->assertInstanceOf(\FMUP\Response\Header::class, $expires);
+        $this->assertInstanceOf(\DateTime::class, $expires->getExpireDate());
+    }
+
+    public function testSetGetExpireDate()
+    {
+        $expires = new \FMUP\Response\Header\Expires();
+        $this->assertSame($expires, $expires->setExpireDate());
+        $this->assertInstanceOf(\DateTime::class, $expires->getExpireDate());
+        $this->assertEquals(new \DateTime(), $expires->getExpireDate());
+        $dateTime = new \DateTime('+2 months');
+        $this->assertSame($expires, $expires->setExpireDate($dateTime));
+        $this->assertInstanceOf(\DateTime::class, $expires->getExpireDate());
+        $this->assertSame($dateTime, $expires->getExpireDate());
+    }
+
+    public function testGetValue()
+    {
+        $expires = new \FMUP\Response\Header\Expires();
+        $dateTime = new \DateTime('+2 months');
+        $this->assertSame($expires, $expires->setExpireDate($dateTime));
+        $this->assertInstanceOf(\DateTime::class, $expires->getExpireDate());
+        $this->assertSame($dateTime->format('D, d M Y H:i:s T'), $expires->getValue());
+    }
+
+    public function testGetType()
+    {
+        $expires = new \FMUP\Response\Header\Expires();
+        $this->assertSame(\FMUP\Response\Header\Expires::TYPE, $expires->getType());
+    }
+}
