dfdce14ad6587f357ae7f86a2b0245ea09f72ed9
jmoulin <jmoulin@castelis.com>
2016-04-27 15:21:14 +0200

    add Response\Header\CacheControl 100% code coverage

---
+++ tests/Response/Header.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Header.php
+ * @author: jmoulin@castelis.com
+ */
+
+namespace Tests\Response;
+
+
+class HeaderTest extends \PHPUnit_Framework_TestCase
+{
+
+}
---
+++ tests/Response/Header/CacheControl.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * CacheControl.php
+ * @author: jmoulin@castelis.com
+ */
+
+namespace Tests\Response\Header;
+
+
+class CacheControlTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $cacheControl = new \FMUP\Response\Header\CacheControl();
+        $this->assertInstanceOf(\DateTime::class, $cacheControl->getExpireDate());
+        $this->assertEquals(time(), $cacheControl->getExpireDate()->getTimestamp());
+        $this->assertSame(\FMUP\Response\Header\CacheControl::CACHE_TYPE_PUBLIC, $cacheControl->getCacheType());
+
+        $cacheControl = new \FMUP\Response\Header\CacheControl(new \DateTime('-1 year'));
+        $this->assertInstanceOf(\DateTime::class, $cacheControl->getExpireDate());
+        $this->assertTrue((time() > $cacheControl->getExpireDate()->getTimestamp()));
+        $this->assertSame(\FMUP\Response\Header\CacheControl::CACHE_TYPE_PUBLIC, $cacheControl->getCacheType());
+
+        $cacheControl = new \FMUP\Response\Header\CacheControl(new \DateTime('-1 year'), \FMUP\Response\Header\CacheControl::CACHE_TYPE_PRIVATE);
+        $this->assertInstanceOf(\DateTime::class, $cacheControl->getExpireDate());
+        $this->assertTrue((time() > $cacheControl->getExpireDate()->getTimestamp()));
+        $this->assertSame(\FMUP\Response\Header\CacheControl::CACHE_TYPE_PRIVATE, $cacheControl->getCacheType());
+    }
+
+    public function testSetGetCacheType()
+    {
+        $cacheControl = new \FMUP\Response\Header\CacheControl();
+        $this->assertSame(\FMUP\Response\Header\CacheControl::CACHE_TYPE_PUBLIC, $cacheControl->getCacheType());
+        $this->assertSame($cacheControl, $cacheControl->setCacheType(\FMUP\Response\Header\CacheControl::CACHE_TYPE_PRIVATE));
+        $this->assertSame(\FMUP\Response\Header\CacheControl::CACHE_TYPE_PRIVATE, $cacheControl->getCacheType());
+        $this->assertSame($cacheControl, $cacheControl->setCacheType('test'));
+        $this->assertSame('test', $cacheControl->getCacheType());
+    }
+
+    public function testSetGetExpireDate()
+    {
+        $cacheControl = new \FMUP\Response\Header\CacheControl();
+        $this->assertInstanceOf(\DateTime::class, $cacheControl->getExpireDate());
+        $this->assertEquals(time(), $cacheControl->getExpireDate()->getTimestamp());
+        $date = new \DateTime('-2 months');
+        $this->assertSame($cacheControl, $cacheControl->setExpireDate($date));
+        $this->assertSame($date, $cacheControl->getExpireDate());
+    }
+
+    public function testGetType()
+    {
+        $cacheControl = new \FMUP\Response\Header\CacheControl();
+        $this->assertSame(\FMUP\Response\Header\CacheControl::TYPE, $cacheControl->getType());
+    }
+
+    public function testGetValue()
+    {
+        $cacheControl = new \FMUP\Response\Header\CacheControl(null, \FMUP\Response\Header\CacheControl::CACHE_TYPE_PRIVATE);
+        $this->assertSame(\FMUP\Response\Header\CacheControl::CACHE_TYPE_PRIVATE . ', max-age=0, must-revalidate', $cacheControl->getValue());
+        $cacheControl = new \FMUP\Response\Header\CacheControl(new \DateTime('+10 seconds'), 'testUnit');
+        $this->assertSame('testUnit, max-age=10, must-revalidate', $cacheControl->getValue());
+    }
+}
