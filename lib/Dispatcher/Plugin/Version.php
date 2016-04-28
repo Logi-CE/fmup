3b5b6afa36ad4945fba4406734b77bda961ee1b2
jmoulin <jmoulin@castelis.com>
2016-04-28 16:52:58 +0200

    add Dispatcher\Plugin\Version 100% code coverage

--- lib/Dispatcher/Plugin/Version.php
+++ lib/Dispatcher/Plugin/Version.php
@@ -8,16 +8,47 @@ namespace FMUP\Dispatcher\Plugin;
 class Version extends \FMUP\Dispatcher\Plugin
 {
     protected $name = 'Version';
+    protected $version;
 
     public function handle()
     {
         $this->getResponse()->setBody(
             preg_replace(
                 '/(<(?:script|link)[^>]+(?:src|href)=["\'][^"\'?]+)(\?[^"\']+)?([\'"])/s',
-                '$1?' . \FMUP\ProjectVersion::getInstance()->get() . '$3',
+                '$1?' . $this->getVersion() . '$3',
                 $this->getResponse()->getBody()
             )
         );
         return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        if (!$this->version) {
+            $this->version = $this->getFromProjectVersion();
+        }
+        return $this->version;
+    }
+
+    /**
+     * @param $version
+     * @return $this
+     */
+    public function setVersion($version)
+    {
+        $this->version = (string) $version;
+        return $this;
+    }
+
+    /**
+     * @return string
+     * @codeCoverageIgnore
+     */
+    protected function getFromProjectVersion()
+    {
+        return \FMUP\ProjectVersion::getInstance()->get();
     }
 }
