3ad32febd7f421f5978c7b10a5182afc1d4e9ceb
jmoulin <jmoulin@castelis.com>
2016-05-02 18:14:38 +0200

    working on channel settings

--- tests/Queue/Channel.php
+++ tests/Queue/Channel.php
@@ -7,7 +7,35 @@
 namespace Tests\Queue;
 
 
-class ChannelTest
+class ChannelTest extends \PHPUnit_Framework_TestCase
 {
+    public function testSetGetSettings()
+    {
+        $channel = new \FMUP\Queue\Channel('test');
+        $this->assertSame('test', $channel->getName());
+        $this->assertSame($channel, $channel->setName('releaseTest'));
+        $this->assertSame('releaseTest', $channel->getName());
+        $this->assertInstanceOf(\FMUP\Queue\Channel\Settings::class, $channel->getSettings());
+        $settings = new \FMUP\Queue\Channel\Settings();
+        $settings->setAutoAck(true);
+        $this->assertEquals($channel, $channel->setSettings($settings));
+        $this->assertSame($settings, $channel->getSettings());
+    }
 
+    public function testGetResourceFail()
+    {
+        $channel = new \FMUP\Queue\Channel('test');
+        $this->expectException(\FMUP\Queue\Exception::class);
+        $this->expectExceptionMessage('Resource must be set before using it');
+        $channel->getResource();
+    }
+
+    public function testSetGetHasResource()
+    {
+        $channel = new \FMUP\Queue\Channel('test');
+        $this->assertFalse($channel->hasResource());
+        $this->assertSame($channel, $channel->setResource('test'));
+        $this->assertTrue($channel->hasResource());
+        $this->assertSame('test', $channel->getResource());
+    }
 }
