82ba57e2a87b200724d06b50ebb52fbf79c158ec
jmoulin <jmoulin@castelis.com>
2016-05-02 18:13:39 +0200

    add Queue\Message 100% code coverage

--- tests/Queue/Message.php
+++ tests/Queue/Message.php
@@ -7,7 +7,16 @@
 namespace Tests\Queue;
 
 
-class MessageTest
+class MessageTest extends \PHPUnit_Framework_TestCase
 {
-
+    public function testSetGetTranslatedOriginal()
+    {
+        $message = new \FMUP\Queue\Message();
+        $this->assertNull($message->getOriginal());
+        $this->assertNull($message->getTranslated());
+        $this->assertSame($message, $message->setOriginal('original'));
+        $this->assertSame('original', $message->getOriginal());
+        $this->assertSame($message, $message->setTranslated('translated'));
+        $this->assertSame('translated', $message->getTranslated());
+    }
 }
