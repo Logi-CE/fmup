133a46bdc256983bf58caa6971713ba20212243e
jmoulin <jmoulin@castelis.com>
2016-05-11 14:30:09 +0200

    add Import/Config/Field/Validator/Required 100% code coverage

---
+++ tests/Import/Config/Field/Validator/Required.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Id.php
+ * @author: jmoulin@castelis.com
+ */
+
+namespace Tests\Import\Config\Field\Validator;
+
+
+class RequiredTest extends \PHPUnit_Framework_TestCase
+{
+    public function testValidate()
+    {
+        $validator = new \FMUP\Import\Config\Field\Validator\Required();
+        $this->assertInstanceOf(\FMUP\Import\Config\Field\Validator::class, $validator);
+        $this->assertFalse($validator->validate(''));
+        $this->assertFalse($validator->validate(false));
+        $this->assertFalse($validator->validate(null));
+        $this->assertTrue($validator->validate(10));
+        $this->assertTrue($validator->validate(-10));
+        $this->assertTrue($validator->validate('test'));
+    }
+
+    public function testGetErrorMessage()
+    {
+        $validator = new \FMUP\Import\Config\Field\Validator\Required();
+        $this->assertInstanceOf(\FMUP\Import\Config\Field\Validator::class, $validator);
+        $this->assertSame('Ce champ est obligatoire mais aucune donnée n\'a été reçue', $validator->getErrorMessage());
+    }
+}
