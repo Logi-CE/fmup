ce3fc79731268d76f0629694f5d6df6d89297022
jmoulin <jmoulin@castelis.com>
2016-05-11 14:05:01 +0200

    add Import/Config/Field/Validator/MaxLength 100% code coverage

---
+++ tests/Import/Config/Field/Validator/MaxLength.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Id.php
+ * @author: jmoulin@castelis.com
+ */
+
+namespace Tests\Import\Config\Field\Validator;
+
+
+class MaxLengthTest extends \PHPUnit_Framework_TestCase
+{
+    public function testValidate()
+    {
+        $validator = new \FMUP\Import\Config\Field\Validator\MaxLength(10);
+        $this->assertInstanceOf(\FMUP\Import\Config\Field\Validator::class, $validator);
+
+        $this->assertTrue($validator->validate('1'));
+        $this->assertTrue($validator->validate('0123456789'));
+        $this->assertFalse($validator->validate('012345678910'));
+        $validator = new \FMUP\Import\Config\Field\Validator\MaxLength(1);
+        $this->assertTrue($validator->validate(''));
+        $this->assertTrue($validator->validate('t'));
+        $this->assertFalse($validator->validate('test'));
+        $this->assertFalse($validator->validate('é'));
+        $validator = new \FMUP\Import\Config\Field\Validator\MaxLength(10);
+        $this->assertTrue($validator->validate('test'));
+        $validator = new \FMUP\Import\Config\Field\Validator\MaxLength(-1);
+        $this->assertFalse($validator->validate(''));
+    }
+
+    public function testGetErrorMessage()
+    {
+        $validator = new \FMUP\Import\Config\Field\Validator\MaxLength(1);
+        $this->assertInstanceOf(\FMUP\Import\Config\Field\Validator::class, $validator);
+        $this->assertSame('Le champ reçu est trop grand', $validator->getErrorMessage());
+    }
+}
